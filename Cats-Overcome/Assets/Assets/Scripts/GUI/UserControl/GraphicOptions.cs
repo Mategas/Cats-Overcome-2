77d1c209a6c96e32cfea3f9684fac6d795cb2a8a
Mategas <mategameg@gmail.com>
2015-01-07 00:32:53 -0300

    Start all over again
    From scratch

---
+++ Cats-Overcome/Assets/Assets/Scripts/GUI/UserControl/GraphicOptions.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphicOptions : MonoBehaviour 
+{
+    public float AntiAliasingSet;
+}
---
+++ Cats-Overcome/Assets/Assets/Scripts/GUI/UserControl/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+
+    bool NewGame = true;
+
+    public void StartGame()
+    {
+        if (NewGame)
+        {
+            NewGame = true;
+            Application.LoadLevel(1);
+        }
+        else
+        {
+            // Here we load the game
+        }
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
